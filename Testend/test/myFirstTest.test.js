f648a0e4db93e2788fc74777384a7b655efc9085
AxelBattut <axelbattut27@gmail.com>
2022-01-09 10:36:33 +0100

    Create myFirstTest.test.js

---
+++ Testend/test/myFirstTest.test.js
@@ -0,0 +1,13 @@
+const assert = require("assert");
+// const { it } = require("mocha");
+// const axios = require("axios");
+
+describe('My first test', function() {
+    it('Test value', async()=>{
+        var myValue = false;
+
+        //TODO
+
+        assert.equal(myValue, myValue);
+    });
+});
