e428a232807354d055365fa1ad724854b10e3035
lilyan <lilyan.bastien@gmail.com>
2022-01-10 19:51:05 +0100

    git prep

--- Testend/test/myFirstTest.test.js
+++ Testend/test/myFirstTest.test.js
@@ -1,12 +1,12 @@
 const assert = require("assert");
-// const { it } = require("mocha");
-// const axios = require("axios");
+//const { it } = require("mocha").it;
+//const { describe } = require("mocha").describe;
+//const axios = require("axios");
 
 describe('My first test', function() {
     it('Test value', async()=>{
+        
         var myValue = false;
-
-        //TODO
 
         assert.equal(myValue, myValue);
     });
