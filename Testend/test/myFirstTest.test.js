7925337c893503fd8582e008e2cdd4dfa22eec8f
AxelBattut <axelbattut27@gmail.com>
2022-01-10 19:54:36 +0100

    Update myFirstTest.test.js

--- Testend/test/myFirstTest.test.js
+++ Testend/test/myFirstTest.test.js
@@ -1,13 +1,57 @@
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+//const assert = require("assert");
+/* const  it  = require("mocha");
+// const axios = require("axios");
+=======
+>>>>>>> Stashed changes
 const assert = require("assert");
 //const { it } = require("mocha").it;
 //const { describe } = require("mocha").describe;
 //const axios = require("axios");
+<<<<<<< Updated upstream
+=======
+>>>>>>> e428a232807354d055365fa1ad724854b10e3035
+
+const  describe = require("mocha");
+>>>>>>> Stashed changes
 
 describe('My first test', function() {
+<<<<<<< HEAD
+    it('Test value', function(){
+=======
     it('Test value', async()=>{
         
+<<<<<<< Updated upstream
+=======
+>>>>>>> e428a232807354d055365fa1ad724854b10e3035
+>>>>>>> Stashed changes
         var myValue = false;
+        
 
         assert.equal(myValue, myValue);
     });
+        
+    
 });
+*/
+
+<<<<<<< Updated upstream
+=======
+const assert = require("assert");
+// const axios = require("axios");
+
+describe('My first test', function() {
+
+    it('Test value', async() => {
+        var myValue = false;
+
+        // TODO
+
+>>>>>>> Stashed changes
+        assert.equal(myValue, myValue);
+    });
+
+});
+
