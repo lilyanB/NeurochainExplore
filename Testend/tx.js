6f54411c9e90aac6650e61dffb2db39d1f25ea87
lilyan <lilyan.bastien@gmail.com>
2022-01-15 14:11:00 +0100

    récup clé

--- Testend/tx.js
+++ Testend/tx.js
@@ -16,7 +16,7 @@ const axios = require('axios');
 axios.get('http://35.181.125.1:8001/api/e2708c6/wallets')
   .then(function (response) {
     // handle success
-    console.log(response);
+    //console.log(response);
   })
   .catch(function (error) {
     // handle error
@@ -24,4 +24,40 @@ axios.get('http://35.181.125.1:8001/api/e2708c6/wallets')
   })
   .then(function () {
     // always executed
-  });
+  })
+
+const fs = require('fs');
+
+
+const clepub = fs.readFileSync('wallet.pub', function read(err, data) {
+    if (err) {
+        throw err;
+    }
+    const priv = data;
+    console.log(priv);
+    
+});
+
+const clepriv = fs.readFileSync('wallet.priv', function(err, data) {
+    if (err) {
+        throw err;
+    }
+    const content = data;
+    console.log(content);
+});
+
+console.log("public key is : " + clepub);
+console.log("private key is : " + clepriv);
+
+const donné = axios.post('http://35.181.125.1:8001/api/e2708c6/wallets', {
+      publicKey: clepub,
+      privateKey: clepriv,
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+ console.log(donné);
