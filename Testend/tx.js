426169e94848fc6009ba56b76f0084a803ccc933
lilyan <lilyan.bastien@gmail.com>
2022-01-25 12:18:59 +0100

    test

--- Testend/tx.js
+++ Testend/tx.js
@@ -31,9 +31,42 @@ console.log("public key is : " + clepub);
 console.log('    ')
 console.log("private key is : " + clepriv);
 
+const clepub2 = fs.readFileSync('wallet2.pub', function read(err, data) {
+  if (err) {
+      throw err;
+  }
+  const priv = data;
+  console.log(priv);
+  
+});
+
+const clepriv2 = fs.readFileSync('wallet2.priv', function(err, data) {
+  if (err) {
+      throw err;
+  }
+  const content = data;
+  console.log(content);
+});
+
+console.log("public key 2 is : " + clepub2);
+console.log('    ')
+console.log("private key 2 is : " + clepriv2);
+
 function tradkey () {
   return new Promise(function (response, reject) {
     axios.post('http://35.181.125.1:8001/api/e2708c6/wallets', {publicKey: clepub,privateKey: clepriv,})
+      .then(function (res) {
+        response(res);
+      })
+      .catch(function (err) {
+        reject(new Error('tradkey failed because ' + err));
+      })
+    });
+  }
+
+function tradkey2 () {
+  return new Promise(function (response, reject) {
+    axios.post('http://35.181.125.1:8001/api/e2708c6/wallets', {publicKey: clepub2,privateKey: clepriv2,})
       .then(function (res) {
         response(res);
       })
@@ -70,6 +103,21 @@ tradkey()
   .catch(function (err) {
     console.error('meaningOfLife() a rapporté une erreur:', err);
   });
+
+tradkey2()
+.then(function (answer) {
+  //console.log('valeur retournée par tradkey():', answer.data);
+  const allKey = answer.data
+  var publicKeytrad = JSON.parse(allKey).publicKey;
+  var privateKeytrad = JSON.parse(allKey).privateKey;
+  console.log("public key 2 is :" + publicKeytrad)
+  console.log('    ')
+  console.log("private key 2 is : " + privateKeytrad)
+  balance(publicKeytrad)
+})
+.catch(function (err) {
+  console.error('meaningOfLife() a rapporté une erreur:', err);
+});
 
 
 
@@ -135,3 +183,18 @@ function riseBalance(aPubKey,rPubKey,val,data,fee) {
       })
     });
 }
+
+
+function tx(aPubKey,rPubKey,val,data,fee) {
+  return new Promise(function (response, reject) {
+    axios.post('http://35.181.125.1:8001/api/e2708c6/transactions', {authorPublicKey: aPubKey, recipientPublicKey: rPubKey, value: val, data: data, fee:fee})
+      .then(function (res) {
+        console.log(res)
+        response(res);
+      })
+      .catch(function (err) {
+        reject(new Error(err));
+      })
+    });
+}
+
