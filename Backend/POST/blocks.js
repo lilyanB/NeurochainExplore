ee1ce0869054fc34982fc5df7a409dffbf20c111
lilyan <lilyan.bastien@gmail.com>
2022-02-20 10:57:26 +0100

    backCheck

--- Backend/POST/blocks.js
+++ Backend/POST/blocks.js
@@ -34,55 +34,22 @@ bodyParser = require('body-parser').json();
 //recup le form POST
 expr.post('/blocks', bodyParser, async (req, res) => {
     console.log("recherche sur la base")
+    let debut = parseInt(req.body.debut);
 
-    //const inputIdTx = req.body.IdTx;
-    //console.log(inputIdTx);
-    const result = await blocks.findOne();
+    const result2 = await blocks.find({ "branchPath.blockNumbers": { $gte: debut } }).sort( { 'branchPath.blockNumbers': 1 }).limit(10);
+    
+    let blockId = [];
+    let pubkey = [];
+    let signature = [];
+    let blockNumber = [];
 
-    const result2 = await blocks.find().sort( { 'branchPath.blockNumbers': 1 }).limit(10);
+    result2.forEach(element => {
+        blockId.push(element.block.header.id.data)
+        pubkey.push(element.block.header.author.keyPub.rawData)
+        signature.push(element.block.header.author.signature.data)
+        blockNumber.push(element.branchPath.blockNumbers)
+    });
 
-    /* 
-    result2.forEach(visible());
-
-    function visible(data) {
-        console.log(data)
-        const blockId = data.block.header.id.data
-        const pubkey = data.blok.header.author.keypub.rawData
-        const signature = data.blok.header.author.signature.rawData 
-    }*/
-
-    const blockId = result2[5].block.header.id.data
-    const pubkey = result2[0].blok.header.author.keypub.rawData
-    const signature = result2[0].blok.header.author.signature.rawData
-
-
-
-    //const bonId = await tx.find({ "blockId.data" : result.block.header.id.data});
-    //console.log(result._id);
-    /*
-    console.log(bonId);
-    const tableau = bonId[0].transaction.outputs;
-    console.log("ceci est le tableau : " + tableau);
-    const nombre = await tx.count({"blockId.data" : result.block.header.id.data}); 
-    console.log("ceci est le nombre : " + nombre);
-
-
-    tx.count({ "blockId.data" : result.block.header.id.data }, function(err, result) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("Number of documents in this ID " + result);
-        }
-      });
-    */
-    /* const bonId = await tx.aggregate([
-        {$match: { _id: ObjectId('6192977acec8d10001488e33') }},
-        {$project:
-            //{Number: { $cond: { if: { $isArray: "$outputs" }, then: { $size: "$outputs" }, else: "NA"} }
-            {nombre: {$size: "$transaction.outputs"}
-         }}
-     ] ) */
-
-    res.json( { result: "result", base: blockId} );
+    res.json( {id: blockId, pubkey: pubkey, signature:signature, blockNumber:blockNumber} );
 
     })
